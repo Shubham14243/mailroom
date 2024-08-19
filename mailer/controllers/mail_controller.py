1034d0aa704a160893235564eb1088c7c4b2ff3d
Shubham Kumar Gupta <126408006+Shubham14243@users.noreply.github.com>
2024-08-19 22:45:06 +0530

    added simple email route

--- mailer/controllers/mail_controller.py
+++ mailer/controllers/mail_controller.py
@@ -4,12 +4,112 @@ from mailer import mail
 from flask_mail import Message
 import datetime
 from mailer.models.templates import Templates
+from mailer.models.mail_log import MailLog
+from mailer.models.app import App
 from mailer.util.validator import Validator
 from mailer.util.user_token import UserToken
 from mailer.config.config import Config
 from mailer.util.mail import Mail
 
 class MailController:
+    
+    @staticmethod
+    def simple_mail(data):
+        
+        try:
+            
+            payload = UserToken.verify_token()
+            
+            if payload['user_id'] == 'exp':
+                response = {
+                    "code": 401,
+                    "status": "failure",
+                    "message": "Token Expired"
+                }
+                return jsonify(response), 401
+            elif payload['user_id'] == 'inv':
+                response = {
+                    "code": 401,
+                    "status": "failure",
+                    "message": "Invalid Token!"
+                }
+                return jsonify(response), 401
+            
+            user_id = payload['user_id']
+            
+            if isinstance(user_id, str):
+                response = {
+                    "code": 400,
+                    "status": "failure",
+                    "message": "Invalid UserID!"
+                }
+                return jsonify(response), 400
+            
+            if Validator.validate_email(data['recipient']) == False or Validator.validate_subject(data['subject']) == False:
+                response = {
+                    "code": 400,
+                    "status": "failure",
+                    "message": "Invalid Data!"
+                }
+                return jsonify(response), 400
+            
+            msg = Message(
+                subject=data['subject'],
+                sender=(data['sender'], Config.MAIL_USERNAME),
+                recipients=[data['recipient']],
+                body=data['body']
+            )
+            
+            mail.send(msg)
+            
+            app = App.query.filter_by(user_id=user_id).first()
+            
+            app_id = app.app_id if app else 0
+            
+            new_log = MailLog(
+                app_id = app_id,
+                template_id = 0,
+                to_email = data['recipient'],
+                from_email = data['sender'],
+                subject = data['subject'],
+                status = "success",
+                sent_at = datetime.datetime.utcnow()
+            )
+            
+            db.session.add(new_log)
+            db.session.commit()
+            
+            response = {
+                "code": 200,
+                "status": "success",
+                "message": "Email Sent Successfully!"
+            }
+            response['recipient'] = data['recipient']
+            response['subject'] = data['subject']
+            
+            return jsonify(response), 200
+        
+        except Exception as e:
+            
+            new_log = MailLog(
+                app_id = app_id,
+                template_id = 0,
+                to_email = data['recipient'],
+                from_email = data['sender'],
+                subject = data['subject'],
+                status = "failure",
+                sent_at = datetime.datetime.utcnow()
+            )
+            
+            db.session.add(new_log)
+            db.session.commit()
+            
+            response = {
+                "code": 500,
+                "status": "error",
+                "message": f"Error Mail Send Controller! {e}"
+            }
+            return jsonify(response), 500
         
     @staticmethod
     def send_mail(data):
@@ -75,6 +175,19 @@ class MailController:
             
             mail.send(msg)
             
+            new_log = MailLog(
+                app_id = template.app_id,
+                template_id = template.template_id,
+                to_email = data['recipient'],
+                from_email = template.sender_email,
+                subject = template.subject,
+                status = "success",
+                sent_at = datetime.datetime.utcnow()
+            )
+            
+            db.session.add(new_log)
+            db.session.commit()
+            
             response = {
                 "code": 200,
                 "status": "success",
@@ -86,6 +199,20 @@ class MailController:
             return jsonify(response), 200
         
         except Exception as e:
+            
+            new_log = MailLog(
+                app_id = template.app_id,
+                template_id = template.template_id,
+                to_email = data['recipient'],
+                from_email = template.sender_email,
+                subject = template.subject,
+                status = "failure",
+                sent_at = datetime.datetime.utcnow()
+            )
+            
+            db.session.add(new_log)
+            db.session.commit()
+            
             response = {
                 "code": 500,
                 "status": "error",
--- mailer/views/mail_view.py
+++ mailer/views/mail_view.py
@@ -3,6 +3,11 @@ from mailer.controllers.mail_controller import MailController
 
 bp = Blueprint('mail', __name__)
 
+@bp.route('/simple', methods=['POST'])
+def simple_mail():
+    data = request.get_json()
+    return MailController.simple_mail(data)
+
 @bp.route('/send', methods=['POST'])
 def send_mail():
     data = request.get_json()
